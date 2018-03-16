214511f00915e6fda0ae8cda22d61b560b35f41e
Kevin <kwang30@gmail.com>
2018-03-16 17:34:01 -0400

    added lucene analyzer

--- src/main/java/IndexMapper.java
+++ src/main/java/IndexMapper.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -9,8 +10,13 @@ import java.util.StringTokenizer;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+
+import opennlp.tools.stemmer.PorterStemmer;
 
 public class IndexMapper extends Mapper< LongWritable, Text, Text, Text> {
+	private Set<String> stopWords = 		(Set<String>) EnglishAnalyzer.getDefaultStopSet();
+
 
 
 
@@ -19,27 +25,20 @@ public class IndexMapper extends Mapper< LongWritable, Text, Text, Text> {
 		String [] line=value.toString().split(",");
 		String content=line[3];
 		String docId=line[0];
-		String words = content.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+		String words = content.toLowerCase().replaceAll("[^a-zA-Z ]", " ");
 		StringTokenizer tokenizer=new StringTokenizer(words);
 		Text file=new Text(docId);
-		Set<String> stopWords = new HashSet<String>();
-
-		String fileLine = null;
-
-		// FileReader reads text files in the default encoding.
-		FileReader fileReader = new FileReader("stopwords.txt");
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-		while((fileLine = bufferedReader.readLine()) != null) {
-			stopWords.add(fileLine);
-		}   
-
-		bufferedReader.close();         
+//		
+//		FileReader reads text files in the default encoding.
+	     
 		while(tokenizer.hasMoreTokens()) {
 			String word=tokenizer.nextToken();
 			if(word.isEmpty()) continue;
 			Text wordText = new Text(word.toString());
+//			PorterStemmer stemmer = new PorterStemmer();
+//			String stem=stemmer.stem(word);
 			if (!stopWords.contains(word)) {
+				System.out.println(word);
 				context.write(wordText, file);
 			}
 		}
