2a9f7d35a8c7cbe9bbe8761614d94a174d82febb
Kevin <kwang30@gmail.com>
2018-04-13 15:42:23 -0400

    add boolea opereators

--- src/main/java/query/Searcher.java
+++ src/main/java/query/Searcher.java
@@ -3,6 +3,7 @@ package query;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Scanner;
 
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
@@ -20,17 +21,22 @@ import scala.Tuple2;
 
 public class Searcher {
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-		String hdfsFile=args[0];
-		if(hdfsFile==null) {
-			hdfsFile="";
-		}
+		Scanner input=new Scanner(System.in);		
+		System.out.println("Enter a term to search on");
+		String word=input.nextLine().trim();
 		
-		String word="all";        
+		System.out.println("Enter a term to not search and filter on");
+		String filter=input.nextLine().trim();
+		
+		System.out.println("Enter a term to also search on");
+		String word2=input.nextLine().trim();
+
+		
         SparkConf conf = new SparkConf().setAppName("Boolean Search").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
        
 //        JavaRDD<String> file = sc.textFile("output");
-        JavaRDD<String> file = sc.textFile(hdfsFile+"/output");
+        JavaRDD<String> file = sc.textFile("output");
     
     
         PairFunction<String, String, String> keyData =
@@ -47,16 +53,17 @@ public class Searcher {
         	Function<Tuple2<String, String>, Boolean> longWordFilter =
         		  new Function<Tuple2<String, String>, Boolean>() {
         		    public Boolean call(Tuple2<String, String> keyValue) {
-        		      return (keyValue._1().contains(word));
+        		    		String term=keyValue._1();
+        		    		return ((term.contains(word) || term.contains(word2)) && !term.contains(filter));
         		    }
         	};
+        	
         	JavaPairRDD<String, String> selectedRows = pairs.filter(longWordFilter);
         	JavaRDD<String> finalResult = selectedRows.map(x -> x._2);
         	finalResult.foreach(data -> {
         		System.out.println(data);
-        	}); 
-
-//        	finalResult.saveAsTextFile("spark_output");
+        	});
+        	finalResult.saveAsTextFile("spark_output");
 
        
 	}
