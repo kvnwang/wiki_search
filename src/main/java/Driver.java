e55f0ed9d696f6f1eaa59c5ffe406ff81011e19f
Kevin <kwang30@gmail.com>
2018-04-12 21:38:47 -0400

    added resuts

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -12,17 +12,13 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-/**
- * @author Shu Lin Chan, Jonathan Maeda, James Wang, Yaoming Zhan
- * Final Project
- */
 public class Driver {
   public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
     Path wiki = new Path(args[0]);
     Path out = new Path(args[1]);
     
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "wiki - g3");
+    Job job = Job.getInstance(conf, "wiki");
 
     TextInputFormat.addInputPath(job, wiki);
     TextOutputFormat.setOutputPath(job, out);
